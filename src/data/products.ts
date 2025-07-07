feece40b68e1877c61ecae45f03374679833b468
erfanhoseinih <erfan.hoseini1379@gmail.com>
2025-07-07 22:02:32 +0330

    add: temp mocka data

---
+++ src/data/products.ts
@@ -0,0 +1,34 @@
+export const configs = [
+  {
+    slug: "luxury-hardbox",
+    title: "جعبه هارد باکس لوکس",
+    image: "/images/products-first/image-1.png",
+    class: "item1",
+    description:
+      "جعبه هارد باکس لوکس با روکش لمینتی براق و طراحی سفارشی، مناسب برای بسته‌بندی هدایای ویژه و محصولات پریمیوم.",
+    subpages: {
+      slug: "luxury-hardbox",
+      title: "جعبه هارد باکس لوکس",
+      image: "/images/products-first/image-1.png",
+      class: "item1",
+      description:
+        "جعبه هارد باکس لوکس با روکش لمینتی براق و طراحی سفارشی، مناسب برای بسته‌بندی هدایای ویژه و محصولات پریمیوم.",
+    },
+  },
+  {
+    slug: "corrugated-box",
+    title: "جعبه مقوایی موج‌دار",
+    image: "/images/products-second/image-2.png",
+    class: "item2",
+    description:
+      "جعبه مقوایی مقاوم با طراحی موج‌دار، ایده‌آل برای بسته‌بندی کالاهای سنگین و ارسال محصولات پستی.",
+  },
+  {
+    slug: "custom-folding-box",
+    title: "جعبه تاشو سفارشی",
+    image: "/images/products-first/image-3.png",
+    class: "item3",
+    description:
+      "جعبه‌های تاشو با قابلیت چاپ اختصاصی، مناسب برای بسته‌بندی محصولات خرده‌فروشی و لوازم آرایشی.",
+  },
+];
