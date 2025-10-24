67957a2ad2c2f59152a873380d3cae7e7d7ec4fb
Mahdi Kamali <49289130+mahdi-kamali@users.noreply.github.com>
2025-10-24 19:56:00 +0330

    fix/rtle

--- src/app/(routes)/[language]/layout.tsx
+++ src/app/(routes)/[language]/layout.tsx
@@ -1,3 +1,4 @@
+"use client";
 import "@/assets/css/index.scss";
 import Footer from "@/components/layouts/Footer/Footer";
 import Header from "@/components/layouts/Header/Header";
@@ -6,26 +7,24 @@ import ReactQueryProvider from "@/app/prividers/ReactQueryProvider";
 import { ToastContainer } from "react-toastify";
 
 import styles from "./layout.module.scss";
+import { useParams } from "next/navigation";
+import { LanguagesENUM } from "@/types/Language/Language.types";
 
 interface IProps {
   children: React.ReactNode;
   params: Promise<{ language: string }>;
 }
 
-export const metadata = {};
 
-export const viewport = {
-  width: "device-width",
-  initialScale: 1,
-};
 
-export default async function RootLayout({ children, params }: IProps) {
-  const { language } = await params;
+export default function RootLayout({ children, params }: IProps) {
+  // const { language } = await params;
+
+  const { language }: { language: LanguagesENUM } = useParams();
+
 
   return (
-    <html
-      lang={language.toUpperCase()}
-      dir='rtl'>
+    <html lang={language.toUpperCase()}>
       <ReactQueryProvider>
         <body className={styles.layout}>
           <Header />
